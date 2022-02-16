33326bdc6713bd8632839783ddae91d41e82c4e9
NatnaelM77 <natnaelmehari77@gmail.com>
2022-02-16 17:02:05 +0300

    binary_tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 
 	if (!new)
-		return NULL;
+		return (NULL);
 
 	if (!parent->left)
 	{
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,12 +14,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *right_child;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 
 	new = malloc(sizeof(binary_tree_t));
 
 	if (!new)
-		return NULL;
+		return (NULL);
 
 	if (!parent->right)
 	{
